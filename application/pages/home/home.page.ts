45719eebce1bf0c0350a0e6e9153921bcee092fc
Oleksandr Khotemskyi <oleksandr.khotemskyi@clearmove.com>
2021-03-24 21:50:44 +0200

    Added step annotation

--- application/pages/home/home.page.ts
+++ application/pages/home/home.page.ts
@@ -1,9 +1,11 @@
+import { step } from "../../utils/allureStep"
 import { TopLinks } from "../components/toplinks.component"
 
 
 export class HomePage {
     topLinks: TopLinks = new TopLinks()
 
+    @step('open all for category')
     openAllForCategory(categoryName: string) {
         $(`a=${categoryName}`).click()
 
---
+++ application/utils/allureStep.ts
@@ -0,0 +1,28 @@
+import allureReporter from '@wdio/allure-reporter'
+
+// startStep(title) - start with a step
+// title (String) - name of the step.
+// endStep(status) - end with a step
+// status (String, optional) - step status, passed by default. Must be "failed", "passed" or "broken"
+
+export function step(msg: string) {
+    return function stepDescriptor(target: any, propertyKey: any, descriptor: any) {
+        const method = descriptor.value;
+        descriptor.value = function descriptorValue(...args) {
+            allureReporter.startStep(msg);
+            try {
+                let result = method.apply(this, args);
+                allureReporter.endStep('passed');
+                return result;
+            } catch (e) {
+                if (e.toString().includes('AssertionError')) {
+                    allureReporter.endStep('failed');
+                } else {
+                    allureReporter.endStep('broken');
+                }
+                throw e;
+            }
+        };
+        return descriptor;
+    };
+}
