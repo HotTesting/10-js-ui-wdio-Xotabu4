e12797b9dcc8b47e9688c82e0c2b3dd6f874c5be
Oleksandr Khotemskyi <oleksandr.khotemskyi@clearmove.com>
2021-03-24 21:28:53 +0200

    silencing spam

--- application/customCommands/click.ts
+++ application/customCommands/click.ts
@@ -4,10 +4,10 @@ export const registerClickFunction = () => {
     browser.overwriteCommand("click", function (origClick, wait = true) {
         // `this` is return value of $(selector)
         if (wait) {
-            console.log('waiting!')
+            // console.log('waiting!')
             expect(this).toBeDisplayed()
         }
-        console.log('clicking!')
+        // console.log('clicking!')
         origClick()
     } as any, true)
 }
