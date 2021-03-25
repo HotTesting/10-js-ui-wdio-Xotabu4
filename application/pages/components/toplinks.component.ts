cb194d42cb8bc1dae4f1627db55a787d9a1185e4
Oleksandr Khotemskyi <Xotabu4@gmail.com>
2021-03-25 22:00:40 +0200

    Update toplinks.component.ts

--- application/pages/components/toplinks.component.ts
+++ application/pages/components/toplinks.component.ts
@@ -1,9 +1,11 @@
+import { step } from "../../utils/allureStep"
 
 export class TopLinks {
     private get root(): WebdriverIO.Element {
         return $('nav#top')
     }
 
+    @step('open checkout')
     openCheckout() {
         this.root.$('a[title="Checkout"]').click()
         browser.pause(1000)
